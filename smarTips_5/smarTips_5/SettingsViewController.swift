7d1e0a3749341dd4f010254d4846796c9cb35561
JeanMenezes <jottamenezes@gmail.com>
2015-12-27 16:17:51 -0800

    basic app + extras
    created gif

--- smarTips_5/smarTips_5/SettingsViewController.swift
+++ smarTips_5/smarTips_5/SettingsViewController.swift
@@ -9,21 +9,149 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-    @IBOutlet weak var tipDefault: UISegmentedControl!
+
+    @IBOutlet weak var setTipStepper: UIStepper!
+    @IBOutlet weak var setTipControl: UISegmentedControl!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("view did load")
+        
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+
+    
+
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+        
+        saveValues()
+    }
+    
+    
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("view did disappear")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
+    }
+    
+    
+   
+    
+    @IBAction func onSetTipControl(sender: UISegmentedControl) {
+        setStepperValue()
+    }
+    
+    
+    @IBAction func onSetTIpStepper(sender: UIStepper) {
+        let newTipPercentage = String(Int(sender.value)) + "%"
+        let currentIndex = setTipControl.selectedSegmentIndex
+        
+        setTipControl.setTitle(newTipPercentage, forSegmentAtIndex: currentIndex)
+    }
+    
+    
+
+    func setStepperValue() {
+        let currentTipPercentage = setTipControl.titleForSegmentAtIndex(setTipControl.selectedSegmentIndex)!
+        let endIndex = currentTipPercentage.endIndex.advancedBy(-1)
+        
+        setTipStepper.value = Double(currentTipPercentage.substringToIndex(endIndex))!
+    }
+    
+    func saveValues() {
+        let tip_low_with_symbol = setTipControl.titleForSegmentAtIndex(0)!
+        let tip_mid_with_symbol = setTipControl.titleForSegmentAtIndex(1)!
+        let tip_high_with_symbol = setTipControl.titleForSegmentAtIndex(2)!
+        
+        //        print("SVC tip percentages with %")
+        //        print(tip_low_with_symbol)
+        //        print(tip_mid_with_symbol)
+        //        print(tip_high_with_symbol)
+        
+        let tip_low = tip_low_with_symbol.substringToIndex(tip_low_with_symbol.endIndex.advancedBy(-1))
+        let tip_mid = tip_mid_with_symbol.substringToIndex(tip_mid_with_symbol.endIndex.advancedBy(-1))
+        let tip_high = tip_high_with_symbol.substringToIndex(tip_high_with_symbol.endIndex.advancedBy(-1))
+        
+        //        print("SVC tip percentages without %")
+        //        print(tip_low)
+        //        print(tip_mid)
+        //        print(tip_high)
+        
+        NSUserDefaults.standardUserDefaults().setObject(String(tip_low), forKey: "tip_low")
+        NSUserDefaults.standardUserDefaults().setObject(String(tip_mid), forKey: "tip_mid")
+        NSUserDefaults.standardUserDefaults().setObject(String(tip_high), forKey: "tip_high")
+        NSUserDefaults.standardUserDefaults().setObject(setTipControl.selectedSegmentIndex, forKey: "selected_segment")
+        
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+    
+    func refreshTipPercentages(){
+        var tipLow = NSUserDefaults.standardUserDefaults().stringForKey("tip_low")
+        var tipMid = NSUserDefaults.standardUserDefaults().stringForKey("tip_mid")
+        var tipHigh = NSUserDefaults.standardUserDefaults().stringForKey("tip_high")
+        
+        //        print("TVC: tip percents without symbol before checking")
+        //        print(tipLow)
+        //        print(tipMid)
+        //        print(tipHigh)
+        
+        if (tipLow == nil ) { tipLow = "18.0" }
+        if (tipMid == nil ) { tipMid = "20.0" }
+        if (tipHigh == nil ) { tipHigh = "22.0" }
+        
+        //        print("TVC: tip percents without symbol after checking")
+        //        print(tipLow)
+        //        print(tipMid)
+        //        print(tipHigh)
+        
+        tipLow! += "%"
+        tipMid! += "%"
+        tipHigh! += "%"
+        
+        //        print("TVC: tip percents with symbol")
+        //        print(tipLow)
+        //        print(tipMid)
+        //        print(tipHigh)
+        
+        setTipControl.setTitle(tipLow, forSegmentAtIndex: 0)
+        setTipControl.setTitle(tipMid, forSegmentAtIndex: 1)
+        setTipControl.setTitle(tipHigh, forSegmentAtIndex: 2)
+        
+    }
+    
+    func selectSegment() {
+        var selectedSegment = NSUserDefaults.standardUserDefaults().stringForKey("selected_segment")
+        
+        if (selectedSegment == nil) { selectedSegment = "0" }
+        
+        setTipControl.selectedSegmentIndex = Int(selectedSegment!)!
+    }
+ 
+    
         // Dispose of any resources that can be recreated.
     }
-    @IBAction func defaultTip(sender: AnyObject) {
-    }
+ 
     
 
     /*
@@ -36,4 +164,4 @@ class SettingsViewController: UIViewController {
     }
     */
 
-}
+
--- smarTips_5/smarTips_5/ViewController.swift
+++ smarTips_5/smarTips_5/ViewController.swift
@@ -16,16 +16,56 @@ class ViewController: UIViewController
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var totalLabel: UILabel!
     
-    
+    var tipPercentages = [0.18, 0.20, 0.22]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("view did load")
         // Do any additional setup after loading the view, typically from a nib.
-        tipLabel.text = "0.00"
-        totalLabel.text = "0.00"
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        
+        let now = NSDate()
+        let before = NSUserDefaults.standardUserDefaults().objectForKey("saved_time") as? NSDate
+        
+        if (before != nil && now.timeIntervalSinceDate(before!) < 600){
+            billField.text = NSUserDefaults.standardUserDefaults().stringForKey("saved_amt")
+        }
         
     }
-
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+        
+        billField.becomeFirstResponder()
+        
+        refreshTipPercentages()
+        selectSegment()
+        calculate()
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+        
+        NSUserDefaults.standardUserDefaults().setObject(NSDate(), forKey: "saved_time")
+        NSUserDefaults.standardUserDefaults().setObject(billField.text, forKey: "saved_amt")
+        NSUserDefaults.standardUserDefaults().setObject(tipControl.selectedSegmentIndex, forKey: "selected_segment")
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("view did disappear")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -33,25 +73,88 @@ class ViewController: UIViewController
 
     @IBAction func onEditingChanged(sender: AnyObject) {
         
-        let tipPercentages = [0.18, 0.20, 0.25]
-        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        let currencyFormatter = NSNumberFormatter()
+        currencyFormatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
+        currencyFormatter.locale = NSLocale.currentLocale()
         
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
         
         let billAmount = NSString(string: billField.text!).doubleValue
         let tip = billAmount * tipPercentage
         let total = billAmount + tip
         
-        tipLabel.text = "\(tip)"
-        totalLabel.text = "\(total)"
+        tipLabel.text = currencyFormatter.stringFromNumber(tip)!
+        totalLabel.text = currencyFormatter.stringFromNumber(total)!
         
-        tipLabel.text = String(format: "$%.2f", tip)
-        
-        totalLabel.text = String(format: "$%.2f", total)
+    
         
         
     }
+    
     @IBAction func onTap(sender: AnyObject)
     {view.endEditing(true)
+    }
+    
+    
+    func calculate() {
+        let currencyFormatter = NSNumberFormatter()
+        currencyFormatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
+        currencyFormatter.locale = NSLocale.currentLocale()
+        
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let billAmount = NSString(string: billField.text!).doubleValue
+        let tip = billAmount * tipPercentage
+        let total = billAmount + tip
+        
+        tipLabel.text = currencyFormatter.stringFromNumber(tip)!
+        totalLabel.text = currencyFormatter.stringFromNumber(total)!
+    }
+    
+    func selectSegment() {
+        var selectedSegment = NSUserDefaults.standardUserDefaults().stringForKey("selected_segment")
+        
+        if (selectedSegment == nil) { selectedSegment = "0" }
+        
+        tipControl.selectedSegmentIndex = Int(selectedSegment!)!
+    }
+    
+    func refreshTipPercentages(){
+        var tipLow = NSUserDefaults.standardUserDefaults().stringForKey("tip_low")
+        var tipMid = NSUserDefaults.standardUserDefaults().stringForKey("tip_mid")
+        var tipHigh = NSUserDefaults.standardUserDefaults().stringForKey("tip_high")
+        
+        //        print("TVC: tip percents without symbol before checking")
+        //        print(tipLow)
+        //        print(tipMid)
+        //        print(tipHigh)
+        
+        if (tipLow == nil ) { tipLow = "18.0" }
+        if (tipMid == nil ) { tipMid = "20.0" }
+        if (tipHigh == nil ) { tipHigh = "22.0" }
+        
+        //        print("TVC: tip percents without symbol after checking")
+        //        print(tipLow)
+        //        print(tipMid)
+        //        print(tipHigh)
+        
+        tipPercentages[0] = NSString(string: tipLow!).doubleValue / 100.0
+        tipPercentages[1] = NSString(string: tipMid!).doubleValue / 100.0
+        tipPercentages[2] = NSString(string: tipHigh!).doubleValue / 100.0
+        
+        tipLow! += "%"
+        tipMid! += "%"
+        tipHigh! += "%"
+        
+        //        print("TVC: tip percents with symbol")
+        //        print(tipLow)
+        //        print(tipMid)
+        //        print(tipHigh)
+        
+        tipControl.setTitle(tipLow, forSegmentAtIndex: 0)
+        tipControl.setTitle(tipMid, forSegmentAtIndex: 1)
+        tipControl.setTitle(tipHigh, forSegmentAtIndex: 2)
+        
     }
 
 }
